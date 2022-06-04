cef3c2a442a78393a0c259c4363e8833b1449862
evribsap <evribsaputra@gmail.com>
2022-06-05 02:46:13 +0700

    update content

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
-function myFunction() {
+function readMore() {
   var dots = document.getElementById("dots");
   var moreText = document.getElementById("more");
   var btnText = document.getElementById("myBtn");
@@ -12,22 +12,4 @@ function myFunction() {
      btnText.innerHTML = "Read less"; 
      moreText.style.display = "inline";
   }
-}
-
-function myFunction1() {
-   document.getElementById("myDropdown").classList.toggle("show");
-}
-  
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-       var openDropdown = dropdowns[i];
-       if (openDropdown.classList.contains('show')) {
-         openDropdown.classList.remove('show');
-       }
-     }
-   }
 }
